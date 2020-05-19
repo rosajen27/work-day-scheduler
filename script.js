8edecc0549fc47fc527a3b874fcbb73165bff4da
Jennifer Rosa <rosajen27@gmail.com>
2020-05-19 12:30:11 -0400

    fixed html and styles

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+// THEN the current day and time is displayed at the top of the calendar
 var currentDay = moment().format('dddd LL');
 $("#currentDay").append(currentDay);
 
 var currentTime = moment().format('LT');
 $("#currentTime").append(currentTime);
+
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
@@ -14,7 +15,6 @@ $("#currentTime").append(currentTime);
 var now = new Date().getHours();
 
 function colorCode() {
-
 if (now > 9) {
     $("#comment9am").addClass("past");
   } else if (now >= 9 && now < 10) {
@@ -88,10 +88,13 @@ if (now > 9) {
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
-
 window.onload = function(){
     colorCode();
+    getCommentItems();
+    setCommentItems();
+}
 
+function getCommentItems() {
     var val9 = localStorage.getItem('#comment9am');
     var val10 = localStorage.getItem('#comment10am');
     var val11 = localStorage.getItem('#comment11am');
@@ -141,15 +144,16 @@ window.onload = function(){
    document.getElementById("comment5pm").value = val5;
 }
 
-window.onbeforeunload = function(){
-    localStorage.setItem('#comment9am', document.getElementById("comment9am").value);
-    localStorage.setItem('#comment10am', document.getElementById("comment10am").value);
-    localStorage.setItem('#comment11am', document.getElementById("comment11am").value);
-    localStorage.setItem('#comment12pm', document.getElementById("comment12pm").value);
-    localStorage.setItem('#comment1pm', document.getElementById("comment1pm").value);
-    localStorage.setItem('#comment2pm', document.getElementById("comment2pm").value);
-    localStorage.setItem('#comment3pm', document.getElementById("comment3pm").value);
-    localStorage.setItem('#comment4pm', document.getElementById("comment4pm").value);
-    localStorage.setItem('#comment5pm', document.getElementById("comment5pm").value);
-}
-
+function setCommentItems() {
+    window.onbeforeunload = function(){
+        localStorage.setItem('#comment9am', document.getElementById("comment9am").value);
+        localStorage.setItem('#comment10am', document.getElementById("comment10am").value);
+        localStorage.setItem('#comment11am', document.getElementById("comment11am").value);
+        localStorage.setItem('#comment12pm', document.getElementById("comment12pm").value);
+        localStorage.setItem('#comment1pm', document.getElementById("comment1pm").value);
+        localStorage.setItem('#comment2pm', document.getElementById("comment2pm").value);
+        localStorage.setItem('#comment3pm', document.getElementById("comment3pm").value);
+        localStorage.setItem('#comment4pm', document.getElementById("comment4pm").value);
+        localStorage.setItem('#comment5pm', document.getElementById("comment5pm").value);
+    }
+    }
